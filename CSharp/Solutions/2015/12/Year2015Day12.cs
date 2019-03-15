495de7d25ba4ef3e2b92c3e6bd2ed12b8403636a
Dmatrix <dimasebakovgoo@gmail.com>
2019-03-15 23:46:07 +0300

    Year: 2015 Day: 12 Part: 2 Language: C#

--- CSharp/Solutions/2015/12/Year2015Day12.cs
+++ CSharp/Solutions/2015/12/Year2015Day12.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using AdventOfCode.Common;
+using Newtonsoft.Json.Linq;
 
 namespace AdventOfCode.Solutions._2015._12
 {
@@ -41,7 +42,46 @@ namespace AdventOfCode.Solutions._2015._12
         public string Part2(IEnumerable<string> lines)
 		{
             var input = GetInput(lines);
-            return "1";
+            return ProcessArray(JArray.Parse(input)).ToString();
+        }
+
+        private static int ProcessArray(JToken jArray)
+        {
+	        return jArray.Children().Sum(ProcessToken);
+        }
+
+        private static int ProcessValue(JValue jValue)
+        {
+	        return int.TryParse(jValue.Value.ToString(), out var elem) ? elem : 0;
+        }
+
+        private static int ProcessObject(JObject jObject)
+        {
+	        foreach (var jProperty in jObject.Properties())
+	        {
+		        if (!(jProperty.Value is JValue jValue)) 
+			        continue;
+		        
+		        if (jValue.Value.ToString() == "red")
+			        return 0;
+	        }
+
+	        return jObject.Properties().Sum(jProperty => ProcessToken(jProperty.Value));
+        }
+
+        private static int ProcessToken(JToken jToken)
+        {
+	        switch (jToken)
+	        {
+		        case JArray jArray:
+			        return ProcessArray(jArray);
+		        case JObject jObjectCase:
+			        return ProcessObject(jObjectCase);
+		        case JValue jValue:
+			        return ProcessValue(jValue);
+	        }
+
+	        return 0;
         }
 	}
 }
