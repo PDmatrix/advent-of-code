46fa018a6c4b72a89c62f2414887df5e45b42ad2
PDmatrix <dimasebakovgoo@gmail.com>
2019-03-30 01:39:53 +0300

    Year 2015 Day 01 Cleanup

--- CSharp/Common/ISolution.cs
+++ CSharp/Common/ISolution.cs
@@ -1,11 +1,10 @@
-using System.Collections;
 using System.Collections.Generic;
 
 namespace AdventOfCode.Common
 {
 	public interface ISolution
 	{
-		string Part1(IEnumerable<string> lines);
-		string Part2(IEnumerable<string> lines);
+		string Part1(IEnumerable<string> input);
+		string Part2(IEnumerable<string> input);
 	}
 }
--- CSharp/Solutions/2015/1/Year2015Day01.cs
+++ CSharp/Solutions/2015/1/Year2015Day01.cs
@@ -8,28 +8,19 @@ namespace AdventOfCode.Solutions._2015._1
 	// ReSharper disable once UnusedMember.Global
 	public class Year2015Day01 : ISolution
 	{
-		private static string Parse(IEnumerable<string> lines)
+		public string Part1(IEnumerable<string> input)
 		{
-			return lines.FirstOrDefault();
+ 			var directions = input.First();
+			return directions.Sum(r => r == '(' ? 1 : -1).ToString();
 		}
 
-		public string Part1(IEnumerable<string> lines)
+		public string Part2(IEnumerable<string> input)
 		{
-			var input = Parse(lines);
+			var directions = input.First();
 			var floor = 0;
-			foreach (var direction in input)
-				floor += direction == '(' ? 1 : -1;
-
-			return floor.ToString();
-		}
-
-		public string Part2(IEnumerable<string> lines)
-		{
-			var input = Parse(lines);
-			var floor = 0;
-			for (var i = 0; i < input.Length; i++)
+			for (var i = 0; i < directions.Length; i++)
 			{
-				floor += input[i] == '(' ? 1 : -1;
+				floor += directions[i] == '(' ? 1 : -1;
 				if (floor == -1)
 					return (i + 1).ToString();
 			}
