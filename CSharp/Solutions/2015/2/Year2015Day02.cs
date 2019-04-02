c705761e2b4f11229f47b2cef91f2db7895a79e3
PDmatrix <dimasebakovgoo@gmail.com>
2019-04-02 20:41:57 +0300

    Year 2015 Day 02 Cleanup

--- CSharp/Solutions/2015/2/Year2015Day02.cs
+++ CSharp/Solutions/2015/2/Year2015Day02.cs
@@ -8,12 +8,12 @@ namespace AdventOfCode.Solutions._2015._2
 	// ReSharper disable once UnusedMember.Global
 	public class Year2015Day02 : ISolution
 	{
-		public string Part1(IEnumerable<string> lines)
+		public string Part1(IEnumerable<string> input)
 		{
 			var result = 0;
-			foreach (var line in lines)
+			foreach (var dimension in input)
 			{
-				var dims = line.Split('x').Select(int.Parse).ToList();
+				var dims = dimension.Split('x').Select(int.Parse).ToList();
 				int l = dims[0],
 					w = dims[1],
 					h = dims[2];
@@ -23,12 +23,12 @@ namespace AdventOfCode.Solutions._2015._2
 			return result.ToString();
 		}
 
-		public string Part2(IEnumerable<string> lines)
+		public string Part2(IEnumerable<string> input)
 		{
 			var result = 0;
-			foreach (var line in lines)
+			foreach (var dimension in input)
 			{
-				var dims = line.Split('x').Select(int.Parse).ToList();
+				var dims = dimension.Split('x').Select(int.Parse).ToList();
 				dims.Sort();
 				result += dims[0] * 2 + dims[1] * 2 + dims.Aggregate((i, w) => i * w);
 			}
