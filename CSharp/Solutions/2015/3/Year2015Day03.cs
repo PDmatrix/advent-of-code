6d0a771c9567b8872340caf1403ea18c4a9d8dfa
PDmatrix <dimasebakovgoo@gmail.com>
2019-04-05 23:05:52 +0300

    Year: 2015 Day: 3

---
+++ CSharp/Solutions/2015/3/Year2015Day03.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Common;
+
+namespace AdventOfCode.Solutions._2015._3
+{
+	// ReSharper disable once UnusedMember.Global
+	public class Year2015Day03 : ISolution
+	{
+		public string Part1(IEnumerable<string> input)
+		{
+			var directions = input.First();
+			var location = new {X = 0, Y = 0};
+			var points = new HashSet<object>{location};
+			foreach (var direction in directions)
+			{
+				if (direction == '>')
+					location = new {X = location.X + 1, location.Y};
+				if (direction == '<')
+					location = new {X = location.X - 1, location.Y};
+				if (direction == '^')
+					location = new {location.X, Y = location.Y + 1};
+				if (direction == 'v')
+					location = new {location.X, Y = location.Y - 1};
+				points.Add(location);
+			}
+			return points.Count.ToString();
+		}
+
+		public string Part2(IEnumerable<string> input)
+		{
+			var directions = input.First();
+			var santa = new {X = 0, Y = 0};
+			var roboSanta = new {X = 0, Y = 0};
+			var santaTurn = true;
+			var points = new HashSet<object>{santa};
+			foreach (var direction in directions)
+			{
+				var location = santaTurn ? santa : roboSanta;
+				if (direction == '>')
+					location = new {X = location.X + 1, location.Y};
+				if (direction == '<')
+					location = new {X = location.X - 1, location.Y};
+				if (direction == '^')
+					location = new {location.X, Y = location.Y + 1};
+				if (direction == 'v')
+					location = new {location.X, Y = location.Y - 1};
+				if (santaTurn)
+					santa = location;
+				else
+					roboSanta = location;
+				santaTurn = !santaTurn;
+				points.Add(location);
+			}
+			return points.Count.ToString();
+		}
+	}
+}
