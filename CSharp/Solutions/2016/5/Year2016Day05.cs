4487d1a1730364688758971829e3ea2130c6b904
PDmatrix <dimasebakovgoo@gmail.com>
2019-04-11 03:53:33 +0300

    Year: 2016 Day: 6

--- CSharp/Solutions/2016/5/Year2016Day05.cs
+++ CSharp/Solutions/2016/5/Year2016Day05.cs
@@ -1,10 +1,7 @@
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Text.RegularExpressions;
 using AdventOfCode.Common;
 
 namespace AdventOfCode.Solutions._2016._5
---
+++ CSharp/Solutions/2016/6/Year2016Day06.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Common;
+
+namespace AdventOfCode.Solutions._2016._6
+{
+    // ReSharper disable once UnusedMember.Global
+    public class Year2016Day06 : ISolution
+    {
+        public string Part1(IEnumerable<string> input)
+        {
+            char MostCommonCharacter(string r) => 
+                r.GroupBy(x => x).OrderByDescending(x => x.Count()).First().Key;
+            
+            var errorCorrectedMessage = 
+                Transpose(input.ToArray()).Select(MostCommonCharacter);
+            return string.Join("", errorCorrectedMessage);
+        }
+
+        public string Part2(IEnumerable<string> input)
+        {
+            char LeastCommonCharacter(string r) => 
+                r.GroupBy(x => x).OrderBy(x => x.Count()).First().Key;
+            
+            var errorCorrectedMessage = 
+                Transpose(input.ToArray()).Select(LeastCommonCharacter);
+            return string.Join("", errorCorrectedMessage);
+        }
+
+        private static IEnumerable<string> Transpose(IReadOnlyList<string> array)
+        {
+            var messageLength = array.First().Length;
+            for (var i = 0; i < messageLength; i++)
+            {
+                yield return array.Aggregate("", (current, t) => current + t[i]);
+            }
+        }
+    }
+}
