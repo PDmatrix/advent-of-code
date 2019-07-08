db836da5554f05c190f19f6c8c7548da73702e8d
PDmatrix <dimasebakovgoo@gmail.com>
2019-07-08 15:48:02 +0300

    Refactoring year 2015 day 13

--- CSharp/Solutions/2015/13/Year2015Day13.cs
+++ CSharp/Solutions/2015/13/Year2015Day13.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using AdventOfCode.Common;
 
 namespace AdventOfCode.Solutions._2015._13
@@ -7,6 +8,98 @@ namespace AdventOfCode.Solutions._2015._13
 	// ReSharper disable once UnusedMember.Global
 	public class Year2015Day13 : ISolution
 	{
+		public string Part1(IEnumerable<string> lines)
+		{
+			var enumerable = lines as string[] ?? lines.ToArray();
+			var guests = GetGuests(enumerable).ToArray();
+			var units = GetUnits(enumerable);
+			var permutations = GetPer(guests);
+			return permutations.Select(r => Compute(r, units)).Max().ToString();
+		}
+
+		public string Part2(IEnumerable<string> lines)
+		{
+			var enumerable = lines as string[] ?? lines.ToArray();
+			var guests = GetGuests(enumerable).ToList();
+			var units = GetUnits(enumerable, true);
+			units["Me"] = new Dictionary<string, int>();
+			foreach (var guest in guests)
+			{
+				units["Me"][guest] = 0;
+			}
+			guests.Add("Me");
+			var permutations = GetPer(guests.ToArray());
+			return permutations.Select(r => Compute(r, units)).Max().ToString();
+		}
+		
+		private static int Compute(IEnumerable<string> list, 
+			IReadOnlyDictionary<string, Dictionary<string, int>> dictionary)
+		{
+			var sum = 0;
+			var enumerable = list as string[] ?? list.ToArray();
+			for (var i = 0; i < enumerable.Length; i++)
+			{
+				if (i == 0)
+					sum += dictionary[enumerable[i]][enumerable.Last()];
+				else
+					sum += dictionary[enumerable[i]][enumerable[i - 1]];
+
+				if (i == enumerable.Length - 1)
+					sum += dictionary[enumerable[i]][enumerable.First()];
+				else
+					sum += dictionary[enumerable[i]][enumerable[i + 1]];
+			}
+
+			return sum;
+		}
+
+		private static IEnumerable<string> GetGuests(IEnumerable<string> lines)
+		{
+			const string pattern = 
+				@"(?<who>\w+) would (lose|gain) (\d+?) happiness units by sitting next to (?<with>\w+)";
+			var res = new List<string>();
+			foreach (var line in lines)
+			{
+				var match = Regex.Match(line, pattern);
+				res.Add(match.Groups["who"].Value);
+				res.Add(match.Groups["with"].Value);
+			}
+
+			return res.Distinct();
+		}
+		
+		private static Dictionary<string, Dictionary<string, int>> GetUnits(IEnumerable<string> lines, bool withMe = false)
+		{
+			const string pattern = 
+				@"(?<who>\w+) would (?<what>lose|gain) (?<by>\d+?) happiness units by sitting next to (?<with>\w+)";
+			var res = new Dictionary<string, Dictionary<string, int>>();
+			foreach (var line in lines)
+			{
+				var match = Regex.Match(line, pattern);
+				var firstGuest = match.Groups["who"].Value;
+				if (!res.ContainsKey(firstGuest))
+				{
+					res[firstGuest] = new Dictionary<string, int>();
+				}
+
+				var secondGuest = match.Groups["with"].Value;
+				
+				if (!res.ContainsKey(secondGuest))
+				{
+					res[secondGuest] = new Dictionary<string, int>();
+				}
+
+				res[firstGuest][secondGuest] =
+					match.Groups["what"].Value == "lose"
+						? -int.Parse(match.Groups["by"].Value)
+						: int.Parse(match.Groups["by"].Value);
+				if (withMe)
+					res[firstGuest]["Me"] = 0;
+			}
+
+			return res;
+		}
+		
 		private static void Swap(ref string a, ref string b)
 		{
 			if (a == b) 
@@ -40,93 +133,6 @@ namespace AdventOfCode.Solutions._2015._13
 				}
 
 			return res;
-		}
-		
-		public string Part1(IEnumerable<string> lines)
-		{
-			var enumerable = lines as string[] ?? lines.ToArray();
-			var guests = GetGuests(enumerable).ToArray();
-			var units = GetUnits(enumerable);
-			var permutations = GetPer(guests);
-			return permutations.Select(r => Compute(r, units)).Max().ToString();
-		}
-
-		private static int Compute(IEnumerable<string> list, 
-			IReadOnlyDictionary<string, Dictionary<string, int>> dictionary)
-		{
-			var sum = 0;
-			var enumerable = list as string[] ?? list.ToArray();
-			for (var i = 0; i < enumerable.Length; i++)
-			{
-				if (i == 0)
-					sum += dictionary[enumerable[i]][enumerable.Last()];
-				else
-					sum += dictionary[enumerable[i]][enumerable[i - 1]];
-
-				if (i == enumerable.Length - 1)
-					sum += dictionary[enumerable[i]][enumerable.First()];
-				else
-					sum += dictionary[enumerable[i]][enumerable[i + 1]];
-			}
-
-			return sum;
-		}
-
-		private static IEnumerable<string> GetGuests(IEnumerable<string> lines)
-		{
-			var res = new List<string>();
-			foreach (var line in lines)
-			{
-				var splitted = line.Split(" ");
-				res.Add(splitted.First());
-				res.Add(splitted.Last().Replace(".", string.Empty));
-			}
-
-			return res.Distinct();
-		}
-		
-		private static Dictionary<string, Dictionary<string, int>> GetUnits(IEnumerable<string> lines, bool withMe = false)
-		{
-			var res = new Dictionary<string, Dictionary<string, int>>();
-			foreach (var line in lines)
-			{
-				var splitted = line.Split(" ");
-				var firstGuest = splitted.First();
-				if (!res.ContainsKey(firstGuest))
-				{
-					res[firstGuest] = new Dictionary<string, int>();
-				}
-
-				var secondGuest = splitted.Last().Replace(".", string.Empty);
-				if (!res.ContainsKey(secondGuest))
-				{
-					res[secondGuest] = new Dictionary<string, int>();
-				}
-
-				res[firstGuest][secondGuest] =
-					splitted[2] == "lose"
-						? int.Parse(splitted[3]) * -1
-						: int.Parse(splitted[3]);
-				if (withMe)
-					res[firstGuest]["Me"] = 0;
-			}
-
-			return res;
-		}
-
-		public string Part2(IEnumerable<string> lines)
-		{
-			var enumerable = lines as string[] ?? lines.ToArray();
-			var guests = GetGuests(enumerable).ToList();
-			var units = GetUnits(enumerable, true);
-			units["Me"] = new Dictionary<string, int>();
-			foreach (var guest in guests)
-			{
-				units["Me"][guest] = 0;
-			}
-			guests.Add("Me");
-			var permutations = GetPer(guests.ToArray());
-			return permutations.Select(r => Compute(r, units)).Max().ToString();
 		}
 	}
 }
