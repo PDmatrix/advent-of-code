86afa24bb8d1d3f603dedecaa314726840860b50
PDmatrix <dimasebakovgoo@gmail.com>
2019-07-24 12:48:27 +0300

    Year: 2016 Day: 16

---
+++ CSharp/Solutions/2016/16/Year2016Day16.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using AdventOfCode.Common;
+
+namespace AdventOfCode.Solutions._2016._16
+{
+    // ReSharper disable once UnusedMember.Global
+    public class Year2016Day16 : ISolution
+    {
+        public string Part1(IEnumerable<string> input)
+        {
+            return CalculateDragonChecksum(input.First(), 272);
+        }
+
+        public string Part2(IEnumerable<string> input)
+        {
+            return CalculateDragonChecksum(input.First(), 35651584);
+        }
+        
+        private static string CalculateDragonChecksum(string initialState, int length)
+        {
+            var current = new StringBuilder(initialState);
+            while (current.Length <= length)
+            {
+                var b = string.Join("", current.ToString().Reverse().Select(r => r == '1' ? '0' : '1'));
+                current.Append("0");
+                current.Append(b);
+            }
+
+            return CalculateChecksum(string.Join("", current.ToString().Take(length)));
+        }
+        
+        private static string CalculateChecksum(string value)
+        {
+            while (true)
+            {
+                if (value.Length % 2 == 1) 
+                    return value;
+                
+                var sb = new StringBuilder();
+                for (var i = 0; i < value.Length; i += 2)
+                {
+                    sb.Append(value[i] == value[i + 1] ? "1" : "0");
+                }
+                value = sb.ToString();
+            }
+        }
+    }
+}
