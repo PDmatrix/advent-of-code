04f3616fdca93c27e931abac1a1eb6b4d9baab40
PDmatrix <dimasebakovgoo@gmail.com>
2019-07-29 13:12:13 +0300

    Year: 2016 Day: 18

---
+++ CSharp/Solutions/2016/18/Year2016Day18.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AdventOfCode.Common;
+
+namespace AdventOfCode.Solutions._2016._18
+{
+    // ReSharper disable once UnusedMember.Global
+    public class Year2016Day18 : ISolution
+    {
+        public string Part1(IEnumerable<string> input)
+        {
+            return GetSafeTiles(input.First(), 40).ToString();
+        }
+
+        public string Part2(IEnumerable<string> input)
+        {
+            return GetSafeTiles(input.First(), 400000).ToString();
+        }
+
+        private static int GetSafeTiles(string initialRow, int rows)
+        {
+            var cur = $".{initialRow}.";
+            var ans = 0;
+            var trapConfig = new List<string> {"^^.", ".^^", "^..", "..^"};
+            ans += cur.Count(c => c == '.') - 2;
+            for (var row = 1; row < rows; row++)
+            {
+                var sb = new StringBuilder();
+                for (var i = 1; i < cur.Length - 1; i++)
+                    sb.Append(trapConfig.Contains(string.Concat(cur[i - 1], cur[i], cur[i + 1])) ? "^" : ".");
+
+                var newPart = sb.ToString(); 
+                ans += newPart.Count(c => c == '.');
+                cur = $".{newPart}.";
+            }
+
+            return ans;
+        }
+    }
+}
