6b7fbbf47ee847c0d63e00f5e060918a5dea5fb3
PDmatrix <dimasebakovgoo@gmail.com>
2019-09-12 07:25:17 +0300

    Year 2017 Day 5

---
+++ CSharp/Solutions/2017/5/Year2017Day05.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Common;
+
+namespace AdventOfCode.Solutions._2017._5
+{
+	// ReSharper disable once UnusedMember.Global
+	public class Year2017Day05 : ISolution
+	{
+		public string Part1(IEnumerable<string> input)
+        {
+            var jumpList = input.Select(int.Parse).ToArray();
+            var idx = 0;
+            var count = 0;
+            while (idx < jumpList.Length)
+            {
+                var prevIdx = idx;
+                idx += jumpList[idx];
+                jumpList[prevIdx]++;
+                count++;
+            }
+            return count.ToString();
+        }
+
+		public string Part2(IEnumerable<string> input)
+        {
+            var jumpList = input.Select(int.Parse).ToArray();
+            var idx = 0;
+            var count = 0;
+            while (idx >= 0 && idx < jumpList.Length)
+            {
+                var prevIdx = idx;
+                idx += jumpList[idx];
+                jumpList[prevIdx] += jumpList[prevIdx] >= 3 ? -1 : 1;
+                count++;
+            }
+            return count.ToString();
+        }
+	}
+}
