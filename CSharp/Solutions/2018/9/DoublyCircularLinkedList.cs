02aefd6ba590e9ab3486fc5b5827fd8565488d74
PDmatrix <dimasebakovgoo@gmail.com>
2020-08-30 19:13:19 +0300

    Year: 2018 Day: 9

---
+++ CSharp/Solutions/2018/9/DoublyCircularLinkedList.cs
@@ -0,0 +1,62 @@
+namespace AdventOfCode.Solutions._2018._9
+{
+    public class Node
+    {
+        public Node(int data)
+        {
+            Data = data;
+        }
+        
+        public long Data;
+        public Node? Next;
+        public Node? Prev;
+    }
+
+    public class DoublyCircularLinkedList
+    {
+        private Node? _first;
+
+        public void InsertAtBeginning(Node newNode)
+        {
+            InsertAtEnd(newNode);
+            _first = newNode;
+        }
+
+        public void InsertAtEnd(Node newNode)
+        {
+            if (_first == null)
+            {
+                _first = newNode;
+                newNode.Next = newNode;
+                newNode.Prev = newNode;
+            }
+            else
+            {
+                InsertAfter(_first.Prev!, newNode);
+            }
+        }
+
+        public void InsertAfter(Node refNode, Node newNode)
+        {
+            newNode.Prev = refNode;
+            newNode.Next = refNode.Next;
+            newNode.Next!.Prev = newNode;
+            refNode.Next = newNode;
+        }
+
+        public void Remove(Node node)
+        {
+            if (_first.Next == _first)
+            {
+                _first = null;
+            }
+            else
+            {
+                node.Prev!.Next = node.Next;
+                node.Next!.Prev = node.Prev;
+                if (_first == node)
+                    _first = node.Next;
+            }
+        }
+    }
+}
---
+++ CSharp/Solutions/2018/9/Year2018Day09.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AdventOfCode.Common;
+
+namespace AdventOfCode.Solutions._2018._9
+{
+	// ReSharper disable once UnusedMember.Global
+	public class Year2018Day09 : ISolution
+	{
+		public string Part1(IEnumerable<string> input)
+		{
+			var groups = Regex.Match(input.First(), @"(\d+) players; last marble is worth (\d+) points").Groups;
+			var players = int.Parse(groups[1].Value);
+			var lastMarble = int.Parse(groups[2].Value);
+
+			return GetWinningScore(players, lastMarble).ToString();
+		}
+
+		private static long GetWinningScore(int players, long lastMarble)
+		{
+			var node = new Node(0);
+			var list = new DoublyCircularLinkedList();
+
+			list.InsertAtBeginning(node);
+			var current = (player: 0, node);
+
+			var playerList = new Dictionary<int, long>();
+			for (var i = 1; i <= players; i++)
+				playerList[i] = 0;
+
+			for (var i = 1; i <= lastMarble; i++)
+			{
+				if (i % 23 == 0)
+				{
+					current = (current.player % players + 1, current.node);
+					playerList[current.player] += i;
+					var prev = current.node;
+					for (var j = 0; j < 7; j++)
+						prev = prev!.Prev;
+
+					playerList[current.player] += prev!.Data;
+					current = (current.player, prev.Next)!;
+					list.Remove(prev);
+					continue;
+				}
+
+				var nd = new Node(i);
+				list.InsertAfter(current.node.Next!, nd);
+				current = (current.player % players + 1, nd);
+			}
+
+			return playerList.Values.Max();
+		}
+
+		public string Part2(IEnumerable<string> input)
+		{
+			var groups = Regex.Match(input.First(), @"(\d+) players; last marble is worth (\d+) points").Groups;
+			var players = int.Parse(groups[1].Value);
+			long lastMarble = int.Parse(groups[2].Value) * 100;
+
+			return GetWinningScore(players, lastMarble).ToString();
+		}
+	}
+}
