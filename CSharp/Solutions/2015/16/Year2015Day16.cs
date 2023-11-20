df6f6dc60104579918ca4faea94ae9c585b8b3ef
PDmatrix <dimasebakovgoo@gmail.com>
2023-11-20 22:45:59 +0300

    Refactor year 2015 day 16

--- CSharp/Solutions/2015/16/Year2015Day16.cs
+++ CSharp/Solutions/2015/16/Year2015Day16.cs
@@ -11,65 +11,30 @@ public class Year2015Day16 : ISolution
 	public object Part1(IEnumerable<string> lines)
 	{
 		var aunts = GetAunts(lines);
-		var etallonAunt = new Dictionary<string, int>
-		{
-			{"children", 3},
-			{"cats", 7},
-			{"samoyeds", 2},
-			{"pomeranians", 3},
-			{"akitas", 0},
-			{"vizslas", 0},
-			{"goldfish", 5},
-			{"trees", 3},
-			{"cars", 2},
-			{"perfumes", 1}
-		};
-		foreach (var aunt in aunts)
-		{
-			var match = true;
-			foreach (var (key, value) in aunt)
-			{
-				if(key == "id")
-					continue;
-
-				if (value == etallonAunt[key]) 
-					continue;
-					
-				match = false;
-				break;
-			}
-
-			if (match)
-				return aunt["id"].ToString();
-		}
-		return "error";
+		var etalonAunt = GetEtalonAunt();
+		return FindRightAunt(aunts, etalonAunt);
 	}
 
 	private static IEnumerable<Dictionary<string, int>> GetAunts(IEnumerable<string> lines)
 	{
-		var res = new List<Dictionary<string, int>>();
 		foreach (var line in lines)
 		{
 			var splitted = line.Split().Select(r =>
 				r.Replace(",", string.Empty)
 					.Replace(":", string.Empty)).ToArray();
-			res.Add(new Dictionary<string, int>
+			yield return new Dictionary<string, int>
 			{
-				{"id", int.Parse(splitted[1])},
-				{splitted[2], int.Parse(splitted[3])},
-				{splitted[4], int.Parse(splitted[5])},
-				{splitted[6], int.Parse(splitted[7])}
-			});
-				
+				{ "id", int.Parse(splitted[1]) },
+				{ splitted[2], int.Parse(splitted[3]) },
+				{ splitted[4], int.Parse(splitted[5]) },
+				{ splitted[6], int.Parse(splitted[7]) }
+			};
 		}
-
-		return res;
 	}
 
-	public object Part2(IEnumerable<string> lines)
+	private static Dictionary<string, int> GetEtalonAunt()
 	{
-		var aunts = GetAunts(lines);
-		var etallonAunt = new Dictionary<string, int>
+		return new Dictionary<string, int>
 		{
 			{"children", 3},
 			{"cats", 7},
@@ -82,45 +47,65 @@ public class Year2015Day16 : ISolution
 			{"cars", 2},
 			{"perfumes", 1}
 		};
+	}
+
+	public object Part2(IEnumerable<string> lines)
+	{
+		var aunts = GetAunts(lines);
+		var etalonAunt = GetEtalonAunt();
+		return FindRightAunt(aunts, etalonAunt, true);
+	}
+
+	private static object FindRightAunt(IEnumerable<Dictionary<string, int>> aunts, IReadOnlyDictionary<string, int> etalonAunt, bool withFixedInstructions = false)
+	{
 		foreach (var aunt in aunts)
 		{
-			var match = true;
-			foreach (var (key, value) in aunt)
-			{
-				if(key == "id")
-					continue;
-					
-				if (key == "trees" || key == "cats")
-				{
-					if (value <= etallonAunt[key])
-					{
-						match = false;
-						break;
-					}
-					continue;
-				}
-					
-				if (key == "pomeranians" || key == "goldfish")
-				{
-					if (value >= etallonAunt[key])
-					{
-						match = false;
-						break;
-					}
-
-					continue;
-				}
-					
-				if (value == etallonAunt[key]) 
-					continue;
-					
-				match = false;
-				break;
-			}
+			var match = MatchAunts(etalonAunt, withFixedInstructions, aunt);
 
 			if (match)
 				return aunt["id"].ToString();
 		}
+
 		return "error";
+	}
+
+	private static bool MatchAunts(IReadOnlyDictionary<string, int> etalonAunt, bool withFixedInstructions, Dictionary<string, int> aunts)
+	{
+		var match = true;
+		foreach (var (key, value) in aunts)
+		{
+			if (key == "id")
+				continue;
+
+			if (withFixedInstructions && (key == "trees" || key == "cats"))
+			{
+				if (value <= etalonAunt[key])
+				{
+					match = false;
+					break;
+				}
+
+				continue;
+			}
+
+			if (withFixedInstructions && (key == "pomeranians" || key == "goldfish"))
+			{
+				if (value >= etalonAunt[key])
+				{
+					match = false;
+					break;
+				}
+
+				continue;
+			}
+
+			if (value == etalonAunt[key])
+				continue;
+
+			match = false;
+			break;
+		}
+
+		return match;
 	}
 }
